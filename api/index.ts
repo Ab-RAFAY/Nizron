8f43ae1b1094e8415c597ef6f063605f282e0d33
abdulrafay <abdulrafay1843@gmail.om>
2026-03-20 00:57:27 +0500

    chore: add vercel serverless function configuration for backend

---
+++ api/index.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { AppModule } from '../src/app.module';
+import { HttpExceptionFilter } from '../src/common/filters/http-exception.filter';
+import { TransformInterceptor } from '../src/common/interceptors/transform.interceptor';
+
+const expressServer = express();
+
+export const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  await app.init();
+  return app;
+};
+
+let cachedServer: any;
+
+export default async (req: any, res: any) => {
+  if (!cachedServer) {
+    cachedServer = await createNestServer(expressServer);
+  }
+  return expressServer(req, res);
+};
