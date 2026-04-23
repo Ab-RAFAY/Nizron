bdad4eb110bf60aaa68ccda3675f265f1e5ef344
abdulrafay <shafqatrasooldev@gmail.com>
2026-04-24 04:19:04 +0500

    feat: configure TanStack Start to use src directory and add entry points

--- nizron-tanstack/app.config.ts
+++ nizron-tanstack/app.config.ts
@@ -4,6 +4,17 @@ import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  tsr: {
+    appDirectory: './src',
+  },
+  routers: {
+    client: {
+      entry: './src/client.tsx',
+    },
+    ssr: {
+      entry: './src/ssr.tsx',
+    }
+  },
   vite: {
     plugins: [
       tsconfigPaths(),
