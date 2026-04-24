06615e2e0b91e194b7d39ea1d1c574b76c2a840a
abdulrafay <shafqatrasooldev@gmail.com>
2026-04-24 14:21:49 +0500

    chore: full upgrade to latest stable TanStack Start and align entry points

--- nizron-tanstack/src/client.tsx
+++ nizron-tanstack/src/client.tsx
@@ -1,5 +1,8 @@
 /// <reference types="vinxi/types/client" />
 import { hydrateRoot } from 'react-dom/client';
 import { StartClient } from '@tanstack/react-start';
+import { createRouter } from './router';
 
-hydrateRoot(document.getElementById('root')!, <StartClient />);
+const router = createRouter();
+
+hydrateRoot(document.getElementById('root')!, <StartClient router={router} />);
--- nizron-tanstack/src/ssr.tsx
+++ nizron-tanstack/src/ssr.tsx
@@ -1,4 +1,11 @@
 /// <reference types="vinxi/types/server" />
 import { createStartHandler, defaultStreamHandler } from '@tanstack/react-start/server';
+import { createRouter } from './router';
 
-export default createStartHandler(defaultStreamHandler);
+export default createStartHandler({
+  createRouter,
+  getRouterManifest: () => {
+    // @ts-ignore
+    return import('tsr:routes-manifest').then((m) => m.default());
+  },
+})(defaultStreamHandler);
